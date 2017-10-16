9907d96e0521e6cf4d4d2e3ad10fbc9f7b027429
zcrome <soportezcrome@gmail.com>
2017-10-15 23:06:55 -0500

    configuracion de login y registro

--- common/models/taxi.js
+++ common/models/taxi.js
@@ -2,4 +2,30 @@
 
 module.exports = function(Taxi) {
 
+
+
+
+	Taxi.afterRemote('login', function( ctx, modelInstance, next) {
+    
+		Taxi.findById(modelInstance.userId, function(err, taxi){
+			if(err){
+				next();
+				return;
+			}
+			modelInstance.code = taxi.code;
+			modelInstance.dni = taxi.dni;
+			modelInstance.name = taxi.name;
+			modelInstance.lastName = taxi.lastName;
+			modelInstance.carName = taxi.carName;
+			modelInstance.carLicensePlate = taxi.carLicensePlate;
+			modelInstance.phone = taxi.phone;
+			modelInstance.email = taxi.email;
+
+			next();
+		})
+    
+	});
+
+
+
 };
