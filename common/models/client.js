619e4e5747a8cf804c7f1510134637e7fc0e0335
zcrome <soportezcrome@gmail.com>
2017-10-16 21:53:11 -0500

    login remote method en cliente

--- common/models/client.js
+++ common/models/client.js
@@ -2,4 +2,28 @@
 
 module.exports = function(Client) {
 
+
+
+Client.afterRemote('login', function( ctx, modelInstance, next) {
+    
+		Client.findById(modelInstance.userId, function(err, client){
+			if(err){
+				next();
+				return;
+			}
+			modelInstance.name = client.name;
+			modelInstance.lastName = client.lastName;			
+			modelInstance.phone = client.phone;
+			modelInstance.email = client.email;
+
+			next();
+		})
+    
+	});
+
+
+
+
+
+
 };
