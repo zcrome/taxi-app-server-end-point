d33a0d2daf37b3c4f080dc0e39d339f9f0ff4ce9
zcrome <soportezcrome@gmail.com>
2017-10-23 00:38:49 -0500

    inclusion de socket io

--- server/boot/root.js
+++ server/boot/root.js
@@ -3,6 +3,67 @@
 module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
+  
+	//Socket io libs
+	var http = require('http');
+	var httpServer = http.createServer(server);
+	var io = require('socket.io').listen(httpServer);
+	//for socket io
+	httpServer.listen(3001);
+
+	//import models
+	var Client = app.models.Client;
+	var Taxi = app.models.Taxi;
+	var TypeEntity = app.models.TypeEntity;
+
+	//global vars
+	var clientsSockets = [];
+	var taxisSockets = [];
+
+	//constans
+	var CLIENT_TYPE_NAME = "CLIENT";
+	var TAXI_TYPE_NAME = "TAXI";
+	var typeClient = {
+		name: CLIENT_TYPE_NAME
+	};
+	var typeTaxi = {
+		name: TAXI_TYPE_NAME
+	};
+
+
+
+	TypeEntity.create(typeClient, function(err, typeCreated){
+		console.log("CLIENT TYPE CREATED");
+	});
+	TypeEntity.create(typeTaxi, function(err, typeCreated){
+		console.log("TAXI TYPE CREATED");
+	});
+
+
+	io.on('connection', function (socket) {
+	  
+	  socket.emit('who-are-you', {});
+	  socket.on('token-Registration', function (data) {
+	  	var type = data.type;
+	  	if(type == CLIENT_TYPE_NAME){
+	  		clientsSockets.push(socket);
+	  	}else if(type == TAXI_TYPE_NAME){
+	  		taxisSockets.push(socket);
+	  	}
+	    socket.emit('connection-aproved', {message: "Bienvenido"});
+	  });
+
+
+	  socket.on('disconnect', function (data) {	   
+	    socket.emit('out', {message: "your re agoin to be disconnected"});
+	    socket.disconnect()
+	  });
+
+
+	});
+
+
+
+
   server.use(router);
 };
--- server/server.js
+++ server/server.js
@@ -2,8 +2,11 @@
 
 var loopback = require('loopback');
 var boot = require('loopback-boot');
-
 var app = module.exports = loopback();
+
+
+
+
 
 app.start = function() {
   // start the web server
@@ -18,6 +21,9 @@ app.start = function() {
   });
 };
 
+
+
+
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname, function(err) {
@@ -27,3 +33,6 @@ boot(app, __dirname, function(err) {
   if (require.main === module)
     app.start();
 });
+
+
+
